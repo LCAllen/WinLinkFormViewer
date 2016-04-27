e6d54bb1541450b15e73bf908e011be82c89a310
Noeva <levicherryallen@live.com>
2016-04-26 21:00:44 -0700

    Format and comment ICS panels

--- src/AppPackage/ICS_205APanel.java
+++ src/AppPackage/ICS_205APanel.java
@@ -14,50 +14,50 @@ import java.awt.Color;
 public class ICS_205APanel extends FormPanel {
 
 	private static final long serialVersionUID = 1L;
-	private JTextField tfIncidentName;
-	
-	private JTextField tfDateTo;
-	private JTextField tfDateFrom;
 
+	// Set up initial variables
+	private JTextField tfIncidentName;
+	private JTextField tfDateFrom;
+	private JTextField tfDateTo;
 	private JTextArea taAssignments;
-	
 	private JTextField tfApprovedName;
 	private JTextField tfApprovedDate;
 
 	public ICS_205APanel() {
 		setLayout(null);
 
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(102, 35, 680, 403);
-		this.add(scrollPane);
-		taAssignments = new JTextArea();
-		scrollPane.setViewportView(taAssignments);
-		taAssignments.setEditable(false);
-		taAssignments.setLineWrap(true);
-
 		// Setup Labels
-		JLabel lblApprovedBy = new JLabel("Approved By");
-		lblApprovedBy.setBounds(10, 449, 82, 14);
-		this.add(lblApprovedBy);
+		JLabel lblIncidentName = new JLabel("Incident Name");
+		lblIncidentName.setBounds(10, 10, 82, 14);
 
-		JLabel lblMessage = new JLabel("Assignments");
-		lblMessage.setBounds(10, 35, 82, 14);
-		this.add(lblMessage);
+		JLabel lblDateTimeFrom = new JLabel("Operational Period: From");
+		lblDateTimeFrom.setBounds(405, 10, 150, 14);
 
 		JLabel lblDateTimeTo = new JLabel("To");
 		lblDateTimeTo.setBounds(656, 10, 22, 14);
-		this.add(lblDateTimeTo);
 
-		JLabel lblIncidentName = new JLabel("Incident Name");
-		lblIncidentName.setBounds(10, 10, 82, 14);
-		this.add(lblIncidentName);
+		JLabel lblMessage = new JLabel("Assignments");
+		lblMessage.setBounds(10, 35, 82, 14);
 
+		JLabel lblApprovedBy = new JLabel("Approved By");
+		lblApprovedBy.setBounds(10, 449, 82, 14);
+		
+		JLabel lblApprovedDate = new JLabel("Date");
+		lblApprovedDate.setBounds(415, 449, 41, 14);
+		
+		// Set up TextFields
 		tfIncidentName = new JTextField();
 		tfIncidentName.setBackground(Color.WHITE);
 		tfIncidentName.setEditable(false);
 		tfIncidentName.setBounds(102, 7, 293, 20);
 		tfIncidentName.setColumns(10);
 
+		tfDateFrom = new JTextField();
+		tfDateFrom.setBackground(Color.WHITE);
+		tfDateFrom.setEditable(false);
+		tfDateFrom.setBounds(552, 7, 93, 20);
+		tfDateFrom.setColumns(10);
+		
 		tfDateTo = new JTextField();
 		tfDateTo.setBackground(Color.WHITE);
 		tfDateTo.setEditable(false);
@@ -70,47 +70,52 @@ public class ICS_205APanel extends FormPanel {
 		tfApprovedName.setBounds(102, 446, 293, 20);
 		tfApprovedName.setColumns(10);
 
-		JLabel lblDateTimeFrom = new JLabel("Operational Period: From");
-		lblDateTimeFrom.setBounds(405, 10, 150, 14);
-		add(lblDateTimeFrom);
-
-		this.add(tfIncidentName);
-		this.add(tfDateTo);
-		this.add(tfApprovedName);
-
-		tfDateFrom = new JTextField();
-		tfDateFrom.setBackground(Color.WHITE);
-		tfDateFrom.setEditable(false);
-		tfDateFrom.setBounds(552, 7, 93, 20);
-		add(tfDateFrom);
-		tfDateFrom.setColumns(10);
-
-		JLabel lblApprovedDate = new JLabel("Date");
-		lblApprovedDate.setBounds(415, 449, 41, 14);
-		add(lblApprovedDate);
-
 		tfApprovedDate = new JTextField();
 		tfApprovedDate.setBackground(Color.WHITE);
 		tfApprovedDate.setEditable(false);
 		tfApprovedDate.setColumns(10);
 		tfApprovedDate.setBounds(456, 446, 326, 20);
-		add(tfApprovedDate);
+		
+		// Set up TextArea + ScrollPane
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(102, 35, 680, 403);
+		taAssignments = new JTextArea();
+		scrollPane.setViewportView(taAssignments);
+		taAssignments.setEditable(false);
+		taAssignments.setLineWrap(true);
+
+		// Add Labels to the panel
+		this.add(lblIncidentName);
+		this.add(lblDateTimeFrom);
+		this.add(lblDateTimeTo);
+		this.add(lblMessage);
+		this.add(lblApprovedBy);
+		this.add(lblApprovedDate);
+		
+		// Add TextFields to the panel
+		this.add(tfIncidentName);
+		this.add(tfDateFrom);
+		this.add(tfDateTo);
+		this.add(scrollPane);
+		this.add(tfApprovedName);
+		this.add(tfApprovedDate);
 	}
 
 	public void parse(File file) {
 
+		// Create report and parser
 		ICS_205AReport report = new ICS_205AReport();
 		ICS_205AParser parser = new ICS_205AParser();
 
+		// Parse the file and put it into an empty report
 		report = parser.parse(file);
 
+		// Set the text fields to the contents of the report
 		try {
 			tfIncidentName.setText(report.incidentName);
 			tfDateFrom.setText(report.dateFrom);
 			tfDateTo.setText(report.dateTo);
-			
 			taAssignments.setText(report.assignments);
-			
 			tfApprovedName.setText(report.approvedName);
 			tfApprovedDate.setText(report.approvedDateTime);
 
--- src/AppPackage/ICS_213Panel.java
+++ src/AppPackage/ICS_213Panel.java
@@ -14,56 +14,46 @@ import java.awt.Color;
 public class ICS_213Panel extends FormPanel {
 
 	private static final long serialVersionUID = 1L;
+	
+	// Set up initial variables
 	private JTextField tfIncidentName;
-	private JTextField tfTo;
 	private JTextField tfFrom;
+	private JTextField tfTo;
 	private JTextField tfSubject;
-	private JTextField tfApprovedPosTitle;
-	private JTextField tfApprovedName;
-
-	private JTextArea taMessage;
 	private JTextField tfDateTime;
+	private JTextArea taMessage;
+	private JTextField tfApprovedName;
+	private JTextField tfApprovedPosTitle;
 
 	public ICS_213Panel() {
 		setLayout(null);
+
+		// Set Up Labels
+		JLabel lblIncidentName = new JLabel("1. Incident Name");
+		lblIncidentName.setBounds(10, 10, 104, 14);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(110, 85, 672, 353);
-		this.add(scrollPane);
-		taMessage = new JTextArea();
-		scrollPane.setViewportView(taMessage);
-		taMessage.setEditable(false);
-		taMessage.setLineWrap(true);
-
-		// Setup Labels
-		JLabel lblApprovedBy = new JLabel("8. Approved By");
-		lblApprovedBy.setBounds(10, 449, 104, 14);
-		this.add(lblApprovedBy);
-
-		JLabel lblApprovedPosTitle = new JLabel("Position / Title");
-		lblApprovedPosTitle.setBounds(405, 449, 82, 14);
-		this.add(lblApprovedPosTitle);
-
-		JLabel lblMessage = new JLabel("7. Message");
-		lblMessage.setBounds(10, 85, 104, 14);
-		this.add(lblMessage);
-
 		JLabel lblTo = new JLabel("2. To");
 		lblTo.setBounds(10, 35, 104, 14);
-		this.add(lblTo);
 
 		JLabel lblFrom = new JLabel("3. From");
 		lblFrom.setBounds(405, 35, 101, 14);
-		this.add(lblFrom);
 
 		JLabel lblSubject = new JLabel("4. Subject");
 		lblSubject.setBounds(10, 60, 104, 14);
-		this.add(lblSubject);
 
-		JLabel lblIncidentName = new JLabel("1. Incident Name");
-		lblIncidentName.setBounds(10, 10, 104, 14);
-		this.add(lblIncidentName);
+		JLabel lblDateTime = new JLabel("5/6. Date / Time");
+		lblDateTime.setBounds(405, 10, 101, 14);
 
+		JLabel lblMessage = new JLabel("7. Message");
+		lblMessage.setBounds(10, 85, 104, 14);
+
+		JLabel lblApprovedBy = new JLabel("8. Approved By");
+		lblApprovedBy.setBounds(10, 449, 104, 14);
+
+		JLabel lblApprovedPosTitle = new JLabel("Position / Title");
+		lblApprovedPosTitle.setBounds(405, 449, 82, 14);
+
+		// Set Up Text Fields
 		tfIncidentName = new JTextField();
 		tfIncidentName.setBackground(Color.WHITE);
 		tfIncidentName.setEditable(false);
@@ -87,45 +77,66 @@ public class ICS_213Panel extends FormPanel {
 		tfSubject.setEditable(false);
 		tfSubject.setBounds(110, 57, 672, 20);
 		tfSubject.setColumns(10);
-
-		tfApprovedPosTitle = new JTextField();
-		tfApprovedPosTitle.setBackground(Color.WHITE);
-		tfApprovedPosTitle.setEditable(false);
-		tfApprovedPosTitle.setBounds(497, 446, 285, 20);
-		tfApprovedPosTitle.setColumns(10);
+		
+		tfDateTime = new JTextField();
+		tfDateTime.setBackground(Color.WHITE);
+		tfDateTime.setEditable(false);
+		tfDateTime.setBounds(497, 7, 285, 20);
+		tfDateTime.setColumns(10);
 
 		tfApprovedName = new JTextField();
 		tfApprovedName.setBackground(Color.WHITE);
 		tfApprovedName.setEditable(false);
 		tfApprovedName.setBounds(110, 446, 285, 20);
 		tfApprovedName.setColumns(10);
+		
+		tfApprovedPosTitle = new JTextField();
+		tfApprovedPosTitle.setBackground(Color.WHITE);
+		tfApprovedPosTitle.setEditable(false);
+		tfApprovedPosTitle.setBounds(497, 446, 285, 20);
+		tfApprovedPosTitle.setColumns(10);	
+		
+		// Create and add the message TextArea + ScrollPane
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(110, 85, 672, 353);
+		taMessage = new JTextArea();
+		scrollPane.setViewportView(taMessage);
+		taMessage.setEditable(false);
+		taMessage.setLineWrap(true);
 
-		JLabel lblDateTime = new JLabel("5/6. Date / Time");
-		lblDateTime.setBounds(405, 10, 101, 14);
-		add(lblDateTime);
-
+		// Add Labels to the panel
+		this.add(lblIncidentName);
+		this.add(lblTo);
+		this.add(lblFrom);
+		this.add(lblSubject);
+		this.add(lblDateTime);
+		this.add(lblMessage);
+		this.add(lblApprovedBy);
+		this.add(lblApprovedPosTitle);
+		
+		// Add TextFields to the panel
 		this.add(tfIncidentName);
 		this.add(tfTo);
 		this.add(tfFrom);
 		this.add(tfSubject);
+		this.add(scrollPane);
+		this.add(tfDateTime);
 		this.add(tfApprovedPosTitle);
 		this.add(tfApprovedName);
 
-		tfDateTime = new JTextField();
-		tfDateTime.setBackground(Color.WHITE);
-		tfDateTime.setEditable(false);
-		tfDateTime.setBounds(497, 7, 285, 20);
-		add(tfDateTime);
-		tfDateTime.setColumns(10);
+
 	}
 
 	public void parse(File file) {
 
+		// Create report and parser
 		ICS_213Report report = new ICS_213Report();
 		ICS_213Parser parser = new ICS_213Parser();
 
+		// Parse the file and put it into an empty report
 		report = parser.parse(file);
 
+		// Set the text fields to the contents of the report
 		try {
 			tfIncidentName.setText(report.incidentName);
 			tfDateTime.setText(report.dateTime);
@@ -134,8 +145,8 @@ public class ICS_213Panel extends FormPanel {
 			tfSubject.setText(report.subject);
 			tfApprovedPosTitle.setText(report.approvedPosTitle);
 			tfApprovedName.setText(report.approvedName);
-
 			taMessage.setText(report.message);
+			
 		} catch (Exception e) {} // Do nothing on thrown exception
 	}
 }
